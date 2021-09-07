3e9dec6f47ad33cb826707f345d28e3ad66783f1
MarufSwe <khanmaruf.swe@gmail.com>
2021-09-08 02:18:36 +0600

    customeInfo page api integrate done

--- src/views/dashboard/CustomInfo.vue
+++ src/views/dashboard/CustomInfo.vue
@@ -50,240 +50,222 @@
   <!--Close header section-->
 
   <div class="container">
-    <h4 class="mt-2">Answering as much as possible provides for a faster turnaround.</h4>
-    <section class="mt-5">
+    <form @submit.prevent="submitCustomInfo" autocomplete="off">
+      <h4 class="mt-2">Answering as much as possible provides for a faster turnaround.</h4>
+      <section class="mt-5">
 
-      <!--License Start-->
-      <div class="col-md-6 mb-2" style="margin-left: -15px;">
-        <label><h5>License#</h5></label>
-        <input
-          v-model="address"
-          type="text"
-          class="form-control"
-          placeholder="license"
-        />
-      </div>
-      <!--License End-->
+        <!--License Start-->
+        <div class="col-md-6 mb-2" style="margin-left: -15px;">
+          <label><h5>License#</h5></label>
+          <input
+            v-model="license"
+            type="text"
+            class="form-control"
+            placeholder="license"
+          />
+        </div>
+        <!--License End-->
 
-      <!--Product Types Start-->
-      <div class="Product Types mt-3">
-        <h5>Product Types:</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">Flower</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Concentrate</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Edibles</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Tinctures</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Vape</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Pre Rolls</label>
-        </div>
-      </div>
-      <!--Product Types End-->
+        <!--Product Types Start-->
+        <div class="Product Types mt-3">
+          <h5>Product Types:</h5>
+          <input type="checkbox" id="jack" value="Flower" v-model="product_type" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">Flower</label>
 
-      <!--Flower Types Start-->
-      <div class="Product Types mt-3">
-        <h5>Flower Type:</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">Indoor</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Outdoor</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Greenhouse</label>
-        </div>
-      </div>
-      <!--Flower Types End-->
+          <input type="checkbox" id="john" value="Concentrate" v-model="product_type" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Concentrate</label>
 
-      <!--Grow method: Start-->
-      <div class="Product Types mt-3">
-        <h5>Product Types:</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">Organic Soil</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Soil</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Hydroponic</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option3">
-          <label class="form-check-label" for="inlineCheckbox3">Organic Hydroponic</label>
-        </div>
-      </div>
-      <!--Grow method: End-->
+          <input type="checkbox" id="Edibles" value="Edibles" v-model="product_type" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Edibles</label>
 
-      <!--Nutrients used Start-->
-      <div class="nutrients-used mt-3">
-        <h5>Nutrients used:</h5>
-        <textarea id="w3review" name="w3review" rows="4" cols="50"></textarea>
-      </div>
-      <!--Nutrients used End-->
+          <input type="checkbox" id="Tinctures" value="Tinctures" v-model="product_type" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Tinctures</label>
 
-      <!--Trim method Start-->
-      <div class="Product Types mt-3">
-        <h5>Trim method:</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">Dry Trim</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Wet Trim</label>
-        </div>
-      </div>
-      <!--Trim method End-->
+          <input type="checkbox" id="Vape" value="Vape" v-model="product_type" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Vape</label>
 
-      <!--Input Filed Start-->
-      <div class="form-group">
-        <div class="form-row  mt-3">
-          <div class="col-md-3">
-            <h5>Curing time (In days)</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
+          <input type="checkbox" id="PreRolls" value="PreRolls" v-model="product_type" style="margin-right: 3px;">
+          <label for="mike">Pre Rolls</label>
+        </div>
+        <!--Product Types End-->
+
+        <!--Flower Types Start-->
+        <div class="Product Types mt-3">
+          <h5>Flower Type:</h5>
+          <input type="checkbox" id="Indoor" value="Indoor" v-model="flower_type" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">Indoor</label>
+
+          <input type="checkbox" id="Outdoor" value="Outdoor" v-model="flower_type" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Outdoor</label>
+
+          <input type="checkbox" id="Greenhouse" value="Greenhouse" v-model="flower_type" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Greenhouse</label>
+        </div>
+        <!--Flower Types End-->
+
+        <!--Grow method: Start-->
+        <div class="Product Types mt-3">
+          <h5>Grow method:</h5>
+          <input type="checkbox" id="OrganicSoil" value="OrganicSoil" v-model="grow_method" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">Organic Soil</label>
+
+          <input type="checkbox" id="Soil" value="Soil" v-model="grow_method" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Soil</label>
+
+          <input type="checkbox" id="Hydroponic" value="Hydroponic" v-model="grow_method" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Hydroponic</label>
+
+          <input type="checkbox" id="OrganicHydroponic" value="OrganicHydroponic" v-model="grow_method"
+                 style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Organic Hydroponic</label>
+        </div>
+        <!--Grow method: End-->
+
+        <!--Nutrients used Start-->
+        <div class="nutrients-used mt-3">
+          <h5>Nutrients used:</h5>
+          <textarea id="w3review" name="w3review" rows="4" cols="50" v-model="nutrients_used"></textarea>
+        </div>
+        <!--Nutrients used End-->
+
+        <!--Trim method Start-->
+        <div class="Product Types mt-3">
+          <h5>Trim method:</h5>
+          <input type="checkbox" id="DryTrim" value="DryTrim" v-model="trim_method" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">Dry Trim</label>
+
+          <input type="checkbox" id="WetTrim" value="WetTrim" v-model="trim_method" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Wet Trim</label>
+        </div>
+        <!--Trim method End-->
+
+        <!--Input Filed Start-->
+        <div class="form-group">
+          <!--Curing & Growing-->
+          <div class="form-row  mt-3">
+            <!--Curing time-->
+            <div class="col-md-3">
+              <h5>Curing time (In days)</h5>
+              <input
+                v-model="curing_time"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
+
+            <!--Growing Location-->
+            <div class="col-md-3">
+              <h5>Growing Location (city)</h5>
+              <input
+                v-model="growing_location"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
           </div>
 
-          <div class="col-md-3">
-            <h5>Growing Location (city)</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
-          </div>
-        </div>
+          <!--Brand Headquarters & Website URL-->
+          <div class="form-row  mt-3">
+            <div class="col-md-3">
+              <h5>Brand Headquarters(city):</h5>
+              <input
+                v-model="brand_headquarters"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
 
-        <div class="form-row  mt-3">
-          <div class="col-md-3">
-            <h5>Brand Headquarters(city):</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
-          </div>
-
-          <div class="col-md-3">
-            <h5>Website URL</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
-          </div>
-        </div>
-
-        <div class="form-row  mt-3">
-          <div class="col-md-3">
-            <h5>Instagram:</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
+            <div class="col-md-3">
+              <h5>Website URL</h5>
+              <input
+                v-model="website_url"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
           </div>
 
-          <div class="col-md-3">
-            <h5>Point of Contact:</h5>
-            <input
-              v-model="outstanding_credit"
-              type="text"
-              class="form-control"
-              placeholder="outstanding credit"
-            />
+          <!--Instagram & Point of Contact-->
+          <div class="form-row  mt-3">
+            <div class="col-md-3">
+              <h5>Instagram:</h5>
+              <input
+                v-model="instagram"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
+
+            <div class="col-md-3">
+              <h5>Point of Contact:</h5>
+              <input
+                v-model="point_of_contact"
+                type="text"
+                class="form-control"
+                placeholder="outstanding credit"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <!--Input Filed End-->
+        <!--Input Filed End-->
 
-      <!--Brand History Start-->
-      <div class="col-md-6 mb-2" style="margin-left: -15px;">
-        <label><h5>Brand History and Mission:</h5></label>
-        <input
-          v-model="address"
-          type="text"
-          class="form-control"
-          placeholder="brand history and mission"
-        />
-      </div>
-      <!--Brand History End-->
-
-      <!--Flower Quality Start-->
-      <div class="Product Types mt-3">
-        <h5>Flower Quality:</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">High End</label>
+        <!--Brand History Start-->
+        <div class="col-md-6 mb-2" style="margin-left: -15px;">
+          <label><h5>Brand History and Mission:</h5></label>
+          <input
+            v-model="brand_history_and_mission"
+            type="text"
+            class="form-control"
+            placeholder="brand history and mission"
+          />
         </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Middle Grade</label>
-        </div>
-      </div>
-      <!--Flower Quality End-->
+        <!--Brand History End-->
 
-      <!--Price point: Start-->
-      <div class="Product Types mt-3">
-        <h5>Price point::</h5>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option1">
-          <label class="form-check-label" for="inlineCheckbox1">High</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Average</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="checkbox" value="option2">
-          <label class="form-check-label" for="inlineCheckbox2">Discounted</label>
-        </div>
-      </div>
-      <!--Price point: End-->
+        <!--Flower Quality Start-->
+        <div class="Product Types mt-3">
+          <h5>Flower Quality:</h5>
+          <input type="checkbox" id="HighEnd" value="HighEnd" v-model="flower_quality" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">High End</label>
 
-      <!--Notes Start-->
-      <div class="nutrients-used mt-3">
-        <h5>Notes:</h5>
-        <textarea name="w3review" rows="4" cols="50"></textarea>
-      </div>
-      <!--Notes End-->
+          <input type="checkbox" id="MiddleGrade" value="MiddleGrade" v-model="flower_quality" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Middle Grade</label>
+        </div>
+        <!--Flower Quality End-->
 
-      <button style="margin-bottom: 30px;"
-        @submit.prevent="submitVendor"
-        class="btn btn-lg btn-primary">
-        Continue
-      </button>
+        <!--Price point: Start-->
+        <div class="Product Types mt-3">
+          <h5>Price point:</h5>
+          <input type="checkbox" id="High" value="High" v-model="price_point" style="margin-right: 3px;">
+          <label for="jack" style="margin-right: 10px;">High</label>
 
-    </section>
+          <input type="checkbox" id="Average" value="Average" v-model="price_point" style="margin-right: 3px;">
+          <label for="john" style="margin-right: 10px;">Average</label>
+
+          <input type="checkbox" id="Discounted" value="Discounted" v-model="price_point" style="margin-right: 3px;">
+          <label for="mike" style="margin-right: 10px;">Discounted</label>
+        </div>
+        <!--Price point: End-->
+
+        <!--Notes Start-->
+        <div class="nutrients-used mt-3">
+          <h5>Notes:</h5>
+          <textarea name="w3review" rows="4" cols="50" v-model="notes"></textarea>
+        </div>
+        <!--Notes End-->
+
+        <button style="margin-bottom: 30px;"
+                @submit.prevent="submitCustomInfo"
+                class="btn btn-lg btn-primary">
+          Continue
+        </button>
+
+      </section>
+    </form>
   </div>
 
   <!--Close Section 7 section-->
@@ -344,8 +326,74 @@
 </template>
 
 <script>
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
 export default {
-  name: 'CustomInfo'
+  name: 'CustomInfo',
+  data () {
+    return {
+
+      // POST API data
+      license: null,
+      // product_type: null,
+      product_type: [],
+      flower_type: [],
+      grow_method: [],
+      nutrients_used: null,
+      trim_method: [],
+      curing_time: null,
+      growing_location: null,
+      brand_headquarters: null,
+      website_url: null,
+      instagram: null,
+      point_of_contact: null,
+      brand_history_and_mission: null,
+      flower_quality: [],
+      price_point: [],
+      notes: null
+
+    }
+  },
+  methods: {
+    async submitCustomInfo () {
+      const bodyParameters = {
+        license: this.license,
+        product_type: this.product_type,
+        flower_type: this.flower_type,
+        grow_method: this.grow_method,
+        nutrients_used: this.nutrients_used,
+        trim_method: this.trim_method,
+        curing_time: this.curing_time,
+        growing_location: this.growing_location,
+        brand_headquarters: this.brand_headquarters,
+        instagram: this.instagram,
+        point_of_contact: this.point_of_contact,
+        brand_history_and_mission: this.brand_history_and_mission,
+        flower_quality: this.flower_quality,
+        price_point: this.price_point,
+        notes: this.notes
+
+      }
+      axios.post('http://127.0.0.1:8000/api/custom-info/',
+        bodyParameters)
+        .then((response) => {
+          Swal.fire({
+            icon: 'success',
+            text: 'Info Added Successfully!'
+          }).then((result) => {
+            // this.$router.go()
+            this.$router.push('education-list')
+            console.log(result)
+          })
+          console.log(response)
+        })
+        .catch((error) => {
+          this.field_validation_data = error.response.data
+          console.log('--++', error.response)
+        })
+    }
+  }
 }
 </script>
 
